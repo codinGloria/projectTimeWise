110ebf9ac851d796551b2622a7d9c59ab7f79b48
Glorinha <liviafariabraz282@gmail.com>
2023-12-12 09:05:48 -0300

    arquivos iniciais

---
+++ backEnd/src/main/java/com/project/liviabraz/TimeWise/TimeWiseApplication.java
@@ -0,0 +1,13 @@
+package com.project.liviabraz.TimeWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeWiseApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/project/liviabraz/TimeWise/TimeWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.liviabraz.TimeWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
